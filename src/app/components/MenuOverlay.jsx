06988743bfc6e82272e7eab0c8ee3c40f6191a55
Loveena28 <loveenaramchandani53@gmail.com>
2024-06-10 14:15:07 +0530

    fixed navbar for small screens

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -4,11 +4,11 @@ import { motion } from "framer-motion";
 
 const variants = {
     open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: "-100%" },
 }
+
 const MenuOverlay = ({ links }) => {
     return (
-        <motion.ul animate = "closed" variants={variants} className="flex flex-col py-4 items-center">
+        <motion.ul initial="closed" animate="open" variants={variants} className="flex flex-col py-4 items-center md:hidden">
             {links.map((link, index) => (
                 <li key={index}>
                     <NavLink href={link.path} title={link.title} />
