97dee0741ac7bcf478de8dc88696c5ee17ec1568
Loveena28 <loveenaramchandani53@gmail.com>
2024-06-10 12:05:25 +0530

    responsive ui fix

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -5,7 +5,7 @@ const projectsData = [
   {
     id: 1,
     title: "VidConnect",
-    description: "A video calling app",
+    description: "Vid Connect is a real-time video conferencing application built with Angular, Socket.io, and PeerJS, enabling seamless video communication with dynamic participant management and intuitive controls.",
     image: "/images/vid-connect.png",
     git: "https://github.com/Loveena28/VidConnect-Frontend",
     preview: "https://vid-connect-theta.vercel.app/"
@@ -13,7 +13,7 @@ const projectsData = [
   {
     id: 2,
     title: "Portfolio",
-    description: "My Personal Porfolio",
+    description: "My personal portfolio: showcasing my projects and skills.",
     image: "/images/portfolio.png",
     git: "https://github.com/Loveena28/my-portfolio",
     preview: "https://my-portfolio-nine-swart-14.vercel.app/",
@@ -21,7 +21,7 @@ const projectsData = [
   {
     id: 3,
     title: "QuickLit",
-    description: "A Book Summarizer",
+    description: "The Book Summary Generator allows users to upload a book cover image, extract the title using Google Cloud Vision API, and generate a detailed summary with AI, which can be saved as a PDF.",
     image: "/images/quicklit.png",
     git: "https://github.com/Loveena28/QuickLit-Frontend",
     preview: "https://quicklit.vercel.app/upload",
--- src/app/components/SkillsSection.jsx
+++ src/app/components/SkillsSection.jsx
@@ -11,55 +11,51 @@ import { TbBrandMysql, TbBrandNextjs } from "react-icons/tb";
 const SkillsSection = () => {
   return (
     <section className="text-white" id="skills">
-      <div className="py-8 px-4 sm:py-16 xl:px-16 mt-4 md:mt-0 text-left flex flex-col items-center h-full">
+      <div className="px-4 pb-16 sm:pb-16 xl:px-16 mt-4 md:mt-0 text-left flex flex-col items-center h-full">
         <h2 className="text-4xl font-bold text-white mb-4 text-center">
           Skills
         </h2>
-        <div className="grid gap-2 grid-cols-2 md:grid-cols-6">
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#E34F26]">
+        <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#E34F26]">
             <FaHtml5 color="black" size={"20"} />
             <p className="text-sm text-center text-black">HTML</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#264DE4]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#264DE4]">
             <FaCss3 color="black" size={"20"} />
             <p className="text-sm text-center text-black">CSS</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#F7DF1E]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#F7DF1E]">
             <FaJs color="black" size={"20"} />
             <p className="text-sm text-center text-black">Javascript</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#3776AB]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#3776AB]">
             <FaPython color="black" size={"20"} />
             <p className="text-sm text-center text-black">Python</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#ff5151]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#ff5151]">
             <FaAngular color="black" size={"20"} />
             <p className="text-sm text-center text-black">Angular</p>
           </div>
-          {/* <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#bbb7b7]">
-            <TbBrandNextjs color="black" size={"20"} />
-            <p className="text-sm text-center text-black">NextJS</p>
-          </div> */}
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#007acc]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#007acc]">
             <BiLogoTypescript color="black" size={"20"} />
             <p className="text-sm text-center text-black">TypeScript</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#bfbfbf]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#bfbfbf]">
             <SiElectron color="black" size={"30"} />
             <p className="text-sm text-center text-black">Electron</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#512bd4]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#512bd4]">
             <p className="text-sm text-center text-black">.NET</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#34A853]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#34A853]">
             <BiLogoGoogleCloud color="black" size={"20"} />
             <p className="text-sm text-center text-black">GCP</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#00758f]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#00758f]">
             <TbBrandMysql color="black" size={"20"} />
             <p className="text-sm text-center text-black">MySQL</p>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center rounded-lg bg-gray-200 shadow-md p-4 mr-4 mb-4 md:mr-8 md:mb-8 hover:bg-[#0064a5]">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md p-6 hover:bg-[#0064a5]">
             <BiLogoPostgresql color="black" size={"20"} />
             <p className="text-sm text-center text-black">PostgreSQL</p>
           </div>
