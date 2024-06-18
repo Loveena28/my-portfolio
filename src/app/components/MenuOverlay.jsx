f1f20e0ffef19b1ac84b36aea08864196af3ffb4
Loveena28 <loveenaramchandani53@gmail.com>
2024-06-18 12:28:59 +0530

    Added 'exit' prop and 'closed' variant to MenuOverlay component

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -4,11 +4,15 @@ import { motion } from "framer-motion";
 
 const variants = {
     open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "-100%" },
 }
 
 const MenuOverlay = ({ links }) => {
     return (
-        <motion.ul initial="closed" animate="open" variants={variants} className="flex flex-col py-4 items-center md:hidden">
+        <motion.ul initial="closed"
+            animate="open"
+            exit="closed"
+            variants={variants} className="flex flex-col py-4 items-center md:hidden">
             {links.map((link, index) => (
                 <li key={index}>
                     <NavLink href={link.path} title={link.title} />
